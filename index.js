e9b85ce56efe1c50af0bcec4cf51bfe22ebc4b4d
Subramaniam <54570377+ajith1717@users.noreply.github.com>
2024-06-12 09:01:16 +0530

    fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 5001;
 const dotenv = require('dotenv');
 dotenv.config();
 
-app.get("/", (req, res) => { res.send("Express on Vercel"); });
+// app.get("/", (req, res) => { res.send("Express on Vercel"); });
 app.listen(port, async () => {
     console.log(`Server is running on Port ${port}`,)
 });

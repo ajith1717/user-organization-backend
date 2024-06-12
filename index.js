887c54f7baa222fc42ecfed963e8641626259de4
Subramaniam <54570377+ajith1717@users.noreply.github.com>
2024-06-12 08:28:59 +0530

    as

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-// declare express
-const express = require("express")
-
+const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
-const path = require("path");
-
-const cors = require('cors');
-//  use route 
-const patientRoute = require("./routes/patients")
-const casesRoute = require("./routes/cases")
-app.use(bodyParser.json({ limit: "20MB" }));
-
-// 👇️ Configure CORS
-app.use(cors());
 app.get("/", (req, res) => { res.send("Express on Vercel"); });
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-app.use("/api/user", patientRoute);
-app.use("/api/case", casesRoute);
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 
 
-
-
-// exports
-module.exports = app
+module.exports = app;

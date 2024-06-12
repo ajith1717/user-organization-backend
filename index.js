fe2626f978e46779602411b2eff0d571f7cfa11d
Subramaniam <54570377+ajith1717@users.noreply.github.com>
2024-06-12 08:27:28 +0530

    push

--- index.js
+++ index.js
@@ -2,7 +2,24 @@
 const express = require("express")
 
 const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const cors = require('cors');
+//  use route 
+const patientRoute = require("./routes/patients")
+const casesRoute = require("./routes/cases")
+app.use(bodyParser.json({ limit: "20MB" }));
+
+// 👇️ Configure CORS
+app.use(cors());
 app.get("/", (req, res) => { res.send("Express on Vercel"); });
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+app.use("/api/user", patientRoute);
+app.use("/api/case", casesRoute);
+
+
 
 
 // exports

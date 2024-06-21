956e7ecad8c1e7f3083452285274cf77013baa0f
Subramaniam <54570377+ajith1717@users.noreply.github.com>
2024-06-21 09:33:59 +0530

    fix: added default value for specialCase

--- models/cases.js
+++ models/cases.js
@@ -81,7 +81,7 @@ const caseSchema = new mongoose.Schema({
     prescription: { type: String, default: "" },
     followUp: { type: String, default: "" },
     returnToEr: { type: String, default: "" },
-    specialCase: { type: String, default: "" },
+    specialCase: { type: String, default: "basic" },
 
 });
 
